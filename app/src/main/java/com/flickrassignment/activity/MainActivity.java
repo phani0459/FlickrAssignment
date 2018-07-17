51527c3bf804d336eed704d4653a4e8869659092
Phani K <phani.0459@gmail.com>
2018-07-17 23:00:36 +0530

    search

--- app/src/main/java/com/flickrassignment/activity/MainActivity.java
+++ app/src/main/java/com/flickrassignment/activity/MainActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -20,13 +24,14 @@ import com.flickrassignment.view.BaseView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements BaseView.MainView {
+public class MainActivity extends AppCompatActivity implements BaseView.MainView, SearchView.OnQueryTextListener {
 
     private ProgressBar progressBar;
     private RecyclerView recyclerView;
 
     private BaseView.presenter presenter;
     private ArrayList<PhotoItem> photoItems;
+    private PhotosAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,25 @@ public class MainActivity extends AppCompatActivity implements BaseView.MainView
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setFocusable(false);
+        searchView.setOnQueryTextListener(this);
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                if (photoItems != null && photoItems.size() > 0) {
+                    adapter = new PhotosAdapter(MainActivity.this, photoItems,recyclerItemClickListener);
+                    recyclerView.setAdapter(adapter);
+                }
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
 
     /**
      * Initializing Toolbar and RecyclerView
@@ -72,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements BaseView.MainView
     @Override
     public void setDataToRecyclerView(ArrayList<PhotoItem> photoItems) {
         this.photoItems = photoItems;
-        PhotosAdapter adapter = new PhotosAdapter(this, photoItems , recyclerItemClickListener);
+        adapter = new PhotosAdapter(this, photoItems , recyclerItemClickListener);
         recyclerView.setAdapter(adapter);
 
     }
@@ -92,5 +116,29 @@ public class MainActivity extends AppCompatActivity implements BaseView.MainView
         presenter.onDestroy();
     }
 
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        if (photoItems != null && photoItems.size() > 0) {
+            if (!TextUtils.isEmpty(query)) {
+                ArrayList<PhotoItem> newPhotoItems = new ArrayList<>();
+                for (int i = 0; i < photoItems.size(); i++) {
+                    if (!TextUtils.isEmpty(photoItems.get(i).getTags()) && photoItems.get(i).getTags().toLowerCase().contains(query.toLowerCase())) {
+                        newPhotoItems.add(photoItems.get(i));
+                    }
+                }
+                adapter = new PhotosAdapter(this, newPhotoItems,recyclerItemClickListener);
+                recyclerView.setAdapter(adapter);
+            } else {
+                adapter = new PhotosAdapter(this, photoItems,recyclerItemClickListener);
+                recyclerView.setAdapter(adapter);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
 }
 
